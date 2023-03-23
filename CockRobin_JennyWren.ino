e82a73df41f677bdea2d8829008a7a7aa367b722
PiperBeaton <128667979+PiperBeaton@users.noreply.github.com>
2023-03-23 00:43:18 -0400

    Update CockRobin_JennyWren.ino

--- CockRobin_JennyWren.ino
+++ CockRobin_JennyWren.ino
@@ -1,17 +1,17 @@
 #include <Servo.h>
 
-Servo myservo1;  // create servo object to control a servo
+Servo myservo1;  // create servo objects to control the servos
 Servo myservo2;
-int servoPin1 = 3;// this pin must be of those with PWM ~
+int servoPin1 = 3;
 int servoPin2 = 5;
 int pushButtonPin1 =2;// the pin where push button is connected
 int pushButtonPin2 = 6;
-int angle =180;    // initial angle  for servo
+int angle =180;    // initial angle  for servos
 int angleStep = 90;
 int angle2 =25;   
 int angleStep2 =25;
-int buttonState = 0; 
-int buttonState2 = 0; // variable for reading the pushbutton status
+int buttonState = 0;  // variables for reading the pushbutton status
+int buttonState2 = 0; 
 
 void setup() {
   myservo1.attach(servoPin1);  // attaches the servo on pin 9 to the servo object
@@ -40,7 +40,7 @@ void loop() {
   myservo1.write(angle); // move the servo to desired angle
 
   delay(200); // waits for the servo to get there
-  }// while
+  }
   if(buttonState2 == HIGH){
   // change the angle for next time through the loop:
     angle2 = angle2 + angleStep2;
@@ -52,6 +52,6 @@ void loop() {
   myservo2.write(angle2); // move the servo to desired angle
 
   delay(200); // waits for the servo to get there
-  }// while
+  }
 }
 
